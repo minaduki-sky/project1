0d6cf3d8d7e6c663bfa1e3bb63384690293d8f17
minaduki-sky <jiaojiemuzhong@gmail.com>
2025-07-11 21:51:08 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+sw.js
+// sw.js
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/register.html",
+        "/style.css",
+        "/icon-192.png",
+        "/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
